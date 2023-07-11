a966874adbaf69cb0fa7a685fe828a1e6f2673fd
mikeee324 <mikeee324@gmail.com>
2023-07-11 18:24:37 +0100

    Dial builds up the UI in editor when something changes now VPM dependency changes

---
+++ Packages/com.vrcstuff.controls/Editor/Dial/DialEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using com.vrcstuff.controls.Dial;
+
+[CustomEditor(typeof(Dial))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if (EditorGUI.EndChangeCheck())
+        {
+            Debug.Log("Text field has changed.");
+            Dial myScript = (Dial)target;
+            myScript._RefreshDialInEditor();
+        }
+    }
+}
